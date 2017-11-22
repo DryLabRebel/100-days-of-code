2e2845cb138a11ed4fe4a833818323961dd72544
Geoff English <geoffrey.english@uqconnect.edu.au>
2017-11-22 15:46:04 +1000

    Update 08prgIDcheckflexi.cpp

--- 08prgIDcheckflexi.cpp
+++ 08prgIDcheckflexi.cpp
@@ -17,7 +17,4 @@ int main()
 	name == "Geoff" || name == "DryLabRebel" ? cout << "\nHi Geoff, What're you up to today?\n\n" : cout << "\n\nWarning!\n\n\n\n\nUnknown user,\n\n\n\n\n\n\n\nInitiate self destruct in:\n\n\n\n3\n\n\n\n2\n\n\n\n1...\n\n\n\nBoom!\n";
 }
 
-// Returns only the 'statement1' output. Not the 'statement2' output, no matter what the input is into 'name'.
-
-
 // End program
