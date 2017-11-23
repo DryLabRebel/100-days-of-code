850fa91431e6cec26cb138d5bb5cae801345f4e3
Geoff English <geoffrey.english@uqconnect.edu.au>
2017-11-23 14:07:44 +1000

    Add files via upload

---
+++ 11IncorrectUser.cpp
@@ -0,0 +1,39 @@
+// 
+// 
+// 
+// Program that continues report incorrect user until correct name is entered
+
+# include <iostream>
+# include <string>
+using namespace std;
+
+int main()
+{
+	string user;
+	int x = 3;
+
+	cout << "Welcome. Please enter your username: ";
+	getline(cin, user);
+
+	if (user == "Geoff" || user == "DryLabRebel")
+		cout << "Hi DryLaRebel, how can I help you today?" << endl;
+
+	else do {
+
+			cout << "Unrecognized user, access denied!" << endl;
+			cout << "Please try again... " << endl;
+			cout << "Attempts remaining: " << x << endl;
+			cout << "Please enter your username: ";
+			getline(cin, user);
+			--x;
+
+		} while ((user != "Geoff" && user != "DryLabRebel") && x > 0);
+
+
+}
+
+
+
+// If user == Geoff || DryLabRebel
+// 	else
+//
